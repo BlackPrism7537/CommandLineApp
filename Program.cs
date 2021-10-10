c76d4fa348b8f906e3723d1c338978a573443798
zachery <z.barcoe@gmail.com>
2021-10-10 18:53:10 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Hello");
+            list.Add("World");
+            list.Add("!");
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
